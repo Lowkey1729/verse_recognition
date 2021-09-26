17bdca16e3c034ab9eda2c68f13ff215bdc39e20
Olarewaju Mojeed <olarewajumojeed9@gmail.com>
2021-09-26 02:02:31 +0100

    new changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
-require('./bootstrap');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routes from './routes';
+import Vuex from 'Vuex';
+import store from './store';
+import Axios from 'axios';
+
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+//tokens
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token');
+if(token){
+    Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
+}
+
+// axios
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+
+
+const app = new Vue({
+    el: '#app',
+    store,
+    router: routes,
+})
